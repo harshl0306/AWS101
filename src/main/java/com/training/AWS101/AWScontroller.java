cd0ebed194424828ea29ce86b2e254149a12a469
Harsh Lavania <harshlofficial36@gmail.com>
2025-05-14 16:20:06 +0530

    First Commit

---
+++ src/main/java/com/training/AWS101/AWScontroller.java
@@ -0,0 +1,13 @@
+package com.training.AWS101;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AWScontroller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "And! Here it is Deployed!";
+    }
+}
---
+++ src/test/java/com/training/AWS101/Aws101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.AWS101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aws101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
